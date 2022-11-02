1e203d1faca25ceb7be6f29671da021a0943a3ce
lucas andrade <lucasandrade@lucass-MacBook-Pro.local>
2022-11-02 12:36:50 +0000

    Initial commit

---
+++ src/main/java/com/db/awmd/challenge/service/AccountsService.java
@@ -0,0 +1,24 @@
+package com.db.awmd.challenge.service;
+
+import com.db.awmd.challenge.domain.Account;
+import com.db.awmd.challenge.repository.AccountsRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountsService {
+
+  @Getter
+  private final AccountsRepository accountsRepository;
+
+  public void createAccount(Account account) {
+    this.accountsRepository.createAccount(account);
+  }
+
+  public Account getAccount(String accountId) {
+    return this.accountsRepository.getAccount(accountId);
+  }
+}
